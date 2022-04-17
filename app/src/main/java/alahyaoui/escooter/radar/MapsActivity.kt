86acd3259f77178c60b4545d2a7d444c9e10c5d4
Lahyaoui Ayoub <Ayoub.Lahyaoui@hotmail.com>
2022-04-17 17:28:24 +0200

    Add askLocationPermission function

--- app/src/main/java/alahyaoui/escooter/radar/MapsActivity.kt
+++ app/src/main/java/alahyaoui/escooter/radar/MapsActivity.kt
@@ -3,19 +3,24 @@ package alahyaoui.escooter.radar
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import alahyaoui.escooter.radar.databinding.ActivityMapsBinding
+import android.Manifest
+import android.annotation.SuppressLint
+import android.os.Build
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
 
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -26,6 +31,36 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        askLocationPermission()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    fun askLocationPermission() {
+        val locationPermissionRequest =
+            registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+                when {
+                    permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
+                        // Precise location access granted.
+                    }
+                    permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
+                        // Only approximate location access granted.
+                    }
+                    else -> {
+                        // No location access granted.
+                    }
+                }
+            }
+
+        // Before you perform the actual permission request, check whether your app
+        // already has the permissions, and whether your app needs to show a permission
+        // rationale dialog. For more details, see Request permissions.
+        locationPermissionRequest.launch(
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        )
     }
 
     /**
@@ -37,12 +72,19 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+    @SuppressLint("MissingPermission")
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+        mMap.isMyLocationEnabled = true
 
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val scooters = mutableListOf<LatLng>()
+        scooters.add(LatLng(50.824393, 4.372279))
+        scooters.add(LatLng(50.821746, 4.426794))
+        scooters.add(LatLng(50.860468, 4.334079))
+
+        for (scooter in scooters) {
+            mMap.addMarker(MarkerOptions().position(scooter).title("Marker of a scooter"))
+        }
     }
 }
