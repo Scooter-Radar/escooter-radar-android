b31608c40592c510c8b54473f445c6ca5b6b1e9c
Lahyaoui Ayoub <Ayoub.Lahyaoui@hotmail.com>
2022-04-18 15:02:34 +0200

    Create Scooter Room entity

---
+++ app/src/main/java/alahyaoui/escooter/radar/database/Scooter.kt
@@ -0,0 +1,37 @@
+package alahyaoui.escooter.radar.database
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.*
+
+@Entity(tableName = "Scooter")
+data class Scooter (
+    @PrimaryKey
+    var id: UUID,
+
+    @ColumnInfo(name = "bike_id")
+    var bikeId: String,
+
+    @ColumnInfo(name = "company")
+    var company: String,
+
+    @ColumnInfo(name = "lat")
+    var latitude: Double,
+
+    @ColumnInfo(name = "lon")
+    var longitude: Double,
+
+    @ColumnInfo(name = "is_disabled")
+    var isDisabled: Boolean,
+
+    @ColumnInfo(name = "is_reserved")
+    var isReserved: Boolean,
+
+    @ColumnInfo(name = "last_reported")
+    var lastReported: Long,
+
+    @ColumnInfo(name = "current_range_meters")
+    var currentRangeMeters: Double,
+)
+
