91f1dffdf0e3c7273604a7f98ec65351a78694c0
Lahyaoui Ayoub <Ayoub.Lahyaoui@hotmail.com>
2022-04-18 19:23:30 +0200

    Remove suspend keyword from getAllByCompany method

--- app/src/main/java/alahyaoui/escooter/radar/models/ScooterDao.kt
+++ app/src/main/java/alahyaoui/escooter/radar/models/ScooterDao.kt
@@ -1,22 +1,36 @@
 package alahyaoui.escooter.radar.models
 
 import androidx.lifecycle.LiveData
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.Query
-import androidx.room.Update
+import androidx.room.*
 
 @Dao
 interface ScooterDao {
-    @Insert
+
+    // INSERT
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(scooter: Scooter)
 
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(scooters: List<Scooter>)
+
+    // UPDATE
     @Update
     suspend fun update(scooter: Scooter)
 
+    @Update
+    suspend fun updateAll(scooters: List<Scooter>)
+
+    // DELETE
+    @Delete
+    fun delete(scooter: Scooter)
+
+    @Delete
+    fun deleteAll()
+
+    // GET
     @Query("SELECT * FROM Scooter")
     fun getAll(): LiveData<List<Scooter>>
 
     @Query("SELECT * from Scooter WHERE company = :company")
-    suspend fun getAllByCompany(company: String): LiveData<List<Scooter>>
+    fun getAllByCompany(company: String): LiveData<List<Scooter>>
 }
