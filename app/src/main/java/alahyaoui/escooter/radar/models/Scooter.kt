688a4d8299729bdd36753979ca4c16883c6abc78
Lahyaoui Ayoub <Ayoub.Lahyaoui@hotmail.com>
2022-04-19 00:24:36 +0200

    Update Scooter model following the changes of the backend Scooter entity

--- app/src/main/java/alahyaoui/escooter/radar/models/Scooter.kt
+++ app/src/main/java/alahyaoui/escooter/radar/models/Scooter.kt
@@ -2,36 +2,45 @@ package alahyaoui.escooter.radar.models
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
-import androidx.room.PrimaryKey
-import java.util.*
+import com.squareup.moshi.Json
 
-@Entity(tableName = "Scooter")
+@Entity(tableName = "Scooter", primaryKeys = ["bike_id", "company"])
 data class Scooter (
-    @PrimaryKey
-    var id: UUID,
 
     @ColumnInfo(name = "bike_id")
+    @Json(name = "bike_id")
     var bikeId: String,
 
     @ColumnInfo(name = "company")
+    @Json(name = "company")
     var company: String,
 
+    @ColumnInfo(name = "zone")
+    @Json(name = "zone")
+    var zone: String,
+
     @ColumnInfo(name = "lat")
+    @Json(name = "lat")
     var latitude: Double,
 
     @ColumnInfo(name = "lon")
+    @Json(name = "lon")
     var longitude: Double,
 
     @ColumnInfo(name = "is_disabled")
+    @Json(name = "is_disabled")
     var isDisabled: Boolean,
 
     @ColumnInfo(name = "is_reserved")
+    @Json(name = "is_reserved")
     var isReserved: Boolean,
 
     @ColumnInfo(name = "last_reported")
+    @Json(name = "last_reported")
     var lastReported: Long,
 
     @ColumnInfo(name = "current_range_meters")
+    @Json(name = "current_range_meters")
     var currentRangeMeters: Double,
 )
 
