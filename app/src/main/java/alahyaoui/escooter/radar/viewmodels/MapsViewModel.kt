6ceaefaae4c4139d6bfb0e4350738d40966ab6fd
Lahyaoui Ayoub <Ayoub.Lahyaoui@hotmail.com>
2022-05-04 17:34:24 +0200

    Add userLocationLiveData attribute, nbOfScootersLiveData attribute and update fetchScootersFromApi method to use getScootersNearLocation instead of getScootersByLocationWithinDegree

--- app/src/main/java/alahyaoui/escooter/radar/viewmodels/MapsViewModel.kt
+++ app/src/main/java/alahyaoui/escooter/radar/viewmodels/MapsViewModel.kt
@@ -2,6 +2,7 @@ package alahyaoui.escooter.radar.viewmodels
 
 import alahyaoui.escooter.radar.models.Scooter
 import alahyaoui.escooter.radar.models.ScooterApi
+import android.location.Location
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -15,12 +16,29 @@ class MapsViewModel() : ViewModel() {
 
     val scootersLiveData: LiveData<List<Scooter>> = _scootersLiveData
 
-    fun fetchScootersFromApi(latitude: Double, longitude: Double, degree: Double){
+    val userLocationLiveData = MutableLiveData<Location>()
+
+    val nbOfScootersLiveData = MutableLiveData<Int>()
+
+    init {
+        nbOfScootersLiveData.value = 100
+    }
+
+    fun fetchScootersFromApi() {
         viewModelScope.launch {
-            try {
-                _scootersLiveData.value = ScooterApi.retrofitService.getScootersByLocationWithinDegree(latitude, longitude, degree)
-            } catch (e: Exception) {
-                _scootersLiveData.value = listOf()
+            val latitude = userLocationLiveData.value?.latitude
+            val longitude = userLocationLiveData.value?.longitude
+            val nbOfScooters = nbOfScootersLiveData.value
+            if (latitude != null && longitude != null && nbOfScooters != null) {
+                try {
+                    _scootersLiveData.value = ScooterApi.retrofitService.getScootersNearLocation(
+                        latitude,
+                        longitude,
+                        nbOfScooters
+                    )
+                } catch (e: Exception) {
+                    _scootersLiveData.value = listOf()
+                }
             }
         }
     }
