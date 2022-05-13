b0941d1cdbb90f0244a54f91a0de35bd133ac8c8
Lahyaoui Ayoub <Ayoub.Lahyaoui@hotmail.com>
2022-05-13 16:34:12 +0200

    Use androidx.navigation.fragment.NavHostFragment instead of androidx.navigation.Navigation.findNavController for fragment navigation

--- app/src/main/java/alahyaoui/escooter/radar/views/MainActivity.kt
+++ app/src/main/java/alahyaoui/escooter/radar/views/MainActivity.kt
@@ -3,16 +3,14 @@ package alahyaoui.escooter.radar.views
 import alahyaoui.escooter.radar.R
 import alahyaoui.escooter.radar.databinding.ActivityMainBinding
 import android.os.Bundle
-import android.view.Menu
-import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
-import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -29,7 +27,8 @@ class MainActivity : AppCompatActivity() {
 
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_content_main) as NavHostFragment
+        val navController = navHostFragment.navController
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
@@ -40,7 +39,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_content_main) as NavHostFragment
+        val navController = navHostFragment.navController
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
