d540a6411dc4f026b489c022bb6da6e14667bf2b
Lahyaoui Ayoub <Ayoub.Lahyaoui@hotmail.com>
2022-05-14 13:41:05 +0200

    Make Location class Parcelable

--- app/src/main/java/alahyaoui/escooter/radar/models/Location.kt
+++ app/src/main/java/alahyaoui/escooter/radar/models/Location.kt
@@ -1,11 +1,14 @@
 package alahyaoui.escooter.radar.models
 
+import android.os.Parcelable
 import com.squareup.moshi.Json
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 data class Location(
     @Json(name = "type")
     var type: String,
 
     @Json(name = "coordinates")
     var coordinates: List<Double>,
-)
+) : Parcelable
