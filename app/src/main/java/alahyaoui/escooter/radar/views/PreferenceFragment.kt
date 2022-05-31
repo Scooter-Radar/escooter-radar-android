b1b504cf8c73b6889bd88ccc5ba97a68d1313090
AyoubLahyaoui <ayoub.lahyaoui@hotmail.com>
2022-05-31 19:41:30 +0200

    Inflate Settings View FrameLayout with PreferenceFragment

---
+++ app/src/main/java/alahyaoui/escooter/radar/views/PreferenceFragment.kt
@@ -0,0 +1,11 @@
+package alahyaoui.escooter.radar.views
+
+import alahyaoui.escooter.radar.R
+import android.os.Bundle
+import androidx.preference.PreferenceFragmentCompat
+
+class PreferenceFragment : PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.preferences, rootKey)
+    }
+}
