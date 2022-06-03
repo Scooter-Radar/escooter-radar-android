ed56a23a1fe8f30e51f5c3ccf0bc8660e856f515
Lahyaoui Ayoub <Ayoub.Lahyaoui@hotmail.com>
2022-06-03 18:56:40 +0200

    Add rent button to ScooterInfoBottomSheet

--- app/src/main/java/alahyaoui/escooter/radar/utils/Constants.kt
+++ app/src/main/java/alahyaoui/escooter/radar/utils/Constants.kt
@@ -7,3 +7,10 @@ object Constants {
 object MapsApiUrls{
     const val directionBaseUrl = "https://www.google.com/maps/dir"
 }
+
+object ScooterApplicationUrls{
+    const val limeUrl = "market://launch?id=com.limebike"
+    const val ponyUrl = "market://launch?id=co.ponybikes.mercury"
+    const val birdUrl = "market://launch?id=co.bird.android"
+    const val spinUrl = "market://launch?id=pm.spin"
+}
