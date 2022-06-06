4060ab1f913863c0a7e5e76d7838f8b2fb10fd1a
Lahyaoui Ayoub <Ayoub.Lahyaoui@hotmail.com>
2022-06-06 16:16:51 +0200

    Use string resource instead of hard coded string

--- app/src/main/java/alahyaoui/escooter/radar/utils/TrackingUtility.kt
+++ app/src/main/java/alahyaoui/escooter/radar/utils/TrackingUtility.kt
@@ -1,5 +1,6 @@
 package alahyaoui.escooter.radar.utils
 
+import alahyaoui.escooter.radar.R
 import android.Manifest
 import android.content.Context
 import androidx.fragment.app.Fragment
@@ -10,15 +11,15 @@ object TrackingUtility {
     fun hasLocationPermissions(context: Context): Boolean {
         return EasyPermissions.hasPermissions(
             context,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
             Manifest.permission.ACCESS_FINE_LOCATION,
-            Manifest.permission.ACCESS_COARSE_LOCATION
         )
     }
 
     fun requestLocationPermissions(fragment: Fragment) {
         EasyPermissions.requestPermissions(
             fragment,
-            "You need to accept location permissions to use this app",
+            fragment.getString(R.string.location_permission_rationale_message),
             PermissionConstants.REQUEST_CODE_LOCATION_PERMISSION,
             Manifest.permission.ACCESS_COARSE_LOCATION,
             Manifest.permission.ACCESS_FINE_LOCATION
