e14248a40bd7333f6505a040759969576a1743d0
Ayoub Lahyaoui <ayoub.lahyaoui@hotmail.com>
2023-05-28 19:56:06 +0200

    Handle no internet connectivity

--- app/src/main/java/alahyaoui/escooter/radar/views/MainActivity.kt
+++ app/src/main/java/alahyaoui/escooter/radar/views/MainActivity.kt
@@ -1,8 +1,12 @@
 package alahyaoui.escooter.radar.views
 
 import alahyaoui.escooter.radar.R
-import alahyaoui.escooter.radar.databinding.ActivityMainBinding
+import alahyaoui.escooter.radar.databinding.MainActivityBinding
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities.NET_CAPABILITY_INTERNET
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
@@ -11,18 +15,16 @@ import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: MainActivityBinding
     private lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = MainActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        drawerLayout = binding.drawerLayout
         val navigationView: NavigationView = binding.navView
-
         navigationView.setNavigationItemSelectedListener {
             val navController = findNavController(R.id.nav_host_fragment_content_main)
             when (it.itemId) {
@@ -32,8 +34,29 @@ class MainActivity : AppCompatActivity() {
             true
         }
 
-        binding.contentView.navDrawerFAB.setOnClickListener {
+        drawerLayout = binding.drawerLayout
+        binding.navDrawerFAB.setOnClickListener {
             drawerLayout.openDrawer(GravityCompat.START)
+        }
+
+        drawLayout()
+        binding.tryAgainButton.setOnClickListener {
+            drawLayout()
+        }
+    }
+
+    fun drawLayout() {
+        if (isNetworkAvailable()) {
+            binding.navView.visibility = View.VISIBLE
+            binding.navDrawerFAB.visibility = View.VISIBLE
+            binding.navHostFragmentContentMain.visibility = View.VISIBLE
+            binding.noInternetLayout.visibility = View.GONE
+        } else {
+            binding.navView.visibility = View.GONE
+            binding.navDrawerFAB.visibility = View.GONE
+            binding.navHostFragmentContentMain.visibility = View.GONE
+            binding.noInternetLayout.visibility = View.VISIBLE
+
         }
     }
 
@@ -43,5 +66,13 @@ class MainActivity : AppCompatActivity() {
         } else {
             super.onBackPressed()
         }
+    }
+
+    private fun isNetworkAvailable(): Boolean {
+        val connectivityManager =
+            getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val capabilities =
+            connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+        return capabilities != null && capabilities.hasCapability(NET_CAPABILITY_INTERNET)
     }
 }
